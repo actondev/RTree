ec9b8d6f202f270321e118f5ff4879f6e8ca4204
actondev <chris.actondev@gmail.com>
2022-03-10 22:38:56 +0100

    WIP test

--- dev.el
+++ dev.el
@@ -10,12 +10,22 @@
 	    (compile "meson compile -C build # --verbose"))
 
 (rtree/defn test
-	    (test-simple-run "meson test -C build -v"))
+	    (let ((debug (aod.transient/flag 'rtree "--debug")))
+	      (if debug
+		  (s7ty/debug-weirdNox-gdb "build/test/run-tests" "")
+		(test-simple-run "meson test -C build -v"))))
+
+(defun rtree/debug-weirdNox-gdb (command args)
+  (gdb-executable command) ; nox fork ;;-i=mi
+  (gdb--command (format "-exec-arguments %s" args))
+  (gdb--command "-exec-run --start"))
 
 (transient-define-prefix rtree ()
   ["rtree"
    ("q" "quit" transient-quit-all)
    ("Q" "quit & restore windows" aod-do/restore :transient transient--do-quit-all)]
+  ["Args"
+   ("-d" "debug" "--debug")]
   ["Commands"
    ("c" "compile" rtree/compile ; :transient transient--do-call
     )
--- test/test.cpp
+++ test/test.cpp
@@ -1,12 +1,51 @@
 #include <catch2/catch.hpp>
 #include <iostream>
+#include <vector>
 
 using std::cout;
 using std::cerr;
 using std::endl;
 
+
+/**
+ * see https://stackoverflow.com/a/49514906/8720686
+ * When calling with <3>(2) will give back
+  0,0,0,
+  0,0,1,
+  0,1,0,
+  0,1,1,
+  1,0,0,
+  1,0,1,
+  1,1,0,
+  1,1,1,
+ * 
+ */
+template<int DIMS>
+std::vector<std::array<float, DIMS>> make_grid(int length) {
+  std::vector<std::array<float, DIMS>> res;
+  int size = pow(length, DIMS);
+  res.reserve(size);
+  for(int i = 0; i < size; i++) {
+    auto& el = res[i];
+    for(int dim = 0; dim < DIMS; dim++) {
+      int denum = pow(length, dim);
+      int x = (int)(i / denum ) % length;
+      int mod = x % length;
+      el[DIMS - dim - 1] = mod;
+    }
+    // cerr << "  ";
+    // for(int i=0; i< DIMS; i++) {
+    //   cerr << el[i] << ",";
+    // }
+    // cerr << endl;
+  }
+
+  return res;
+}
+
+
 TEST_CASE("rtree test init") {
-
   cerr << "hi there\n";
-
+  auto grid = make_grid<3>(2);
+  cerr << "done?\n";
 }
