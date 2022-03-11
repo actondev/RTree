b3c8424a8bbaa58f66c3dffe6950b9b627a5fbbc
actondev <chris.actondev@gmail.com>
2022-03-11 20:26:45 +0100

    more test & cleanup

--- test/test.cpp
+++ test/test.cpp
@@ -15,46 +15,17 @@ std::string pp(const T& x)
   return easyprint::stringify(x);
 }
 
+using Point = std::vector<double>;
+using Grid = std::vector<Point>;
+
 /**
  * see https://stackoverflow.com/a/49514906/8720686
- * When calling with <3>(2) will give back
-  0,0,0,
-  0,0,1,
-  0,1,0,
-  0,1,1,
-  1,0,0,
-  1,0,1,
-  1,1,0,
-  1,1,1,
- *
+ * Given length=3,dims=2 it gives
+ * {{0, 0}, {0, 1}, {0, 2},
+ *  {1, 0}, {1, 1}, {1, 2},
+ *  {2, 0}, {2, 1}, {2, 2}}
  */
-template <int DIMS>
-std::vector<std::array<double, DIMS>> make_grid_template(int length)
-{
-  std::vector<std::array<double, DIMS>> res;
-  int size = pow(length, DIMS);
-  res.reserve(size);
-  for (int i = 0; i < size; i++)
-  {
-    auto &el = res[i];
-    for (int dim = 0; dim < DIMS; dim++)
-    {
-      int denum = pow(length, dim);
-      int x = (int)(i / denum) % length;
-      int mod = x % length;
-      el[DIMS - dim - 1] = mod;
-    }
-    cerr << "  ";
-    for(int i=0; i< DIMS; i++) {
-      cerr << el[i] << ",";
-    }
-    cerr << endl;
-  }
-
-  return res;
-}
-
-std::vector<std::vector<double>> make_grid(int length, int dims = 2)
+Grid make_grid(int length, int dims = 2)
 {
   std::vector<std::vector<double>> res;
   int size = pow(length, dims);
@@ -62,7 +33,8 @@ std::vector<std::vector<double>> make_grid(int length, int dims = 2)
   for (int i = 0; i < size; i++)
   {
     auto &el = res[i];
-    el.resize(dims); // resize or reserve? kind of same?
+    // not optimal, but makes things simple (no template, runtime only)
+    el.resize(dims);
     for (int dim = 0; dim < dims; dim++)
     {
       int denum = pow(length, dim);
@@ -70,91 +42,69 @@ std::vector<std::vector<double>> make_grid(int length, int dims = 2)
       int mod = x % length;
       el[dims - dim - 1] = mod;
     }
-    cerr << "  ";
-    for(int i=0; i< dims; i++) {
-      cerr << el[i] << ",";
-    }
-    cerr << endl;
   }
 
   return res;
 }
 
-// template<typename T>
-// void print_vec(std::ostream &os, const std::vector<T> &vec) {
-//   auto size = vec.size();
-//   // os << "[" << size << "]";
-//   os << "{";
-//   for(auto i=0; i < size - 1; i++) {
-//     os << vec[i] << ",";
-//   }
-//   if(size > 0) {
-//     os << vec[size-1];
-//   }
-//   os << "}";
-// }
+template<int DIMS>
+RTree<Point, double, DIMS> grid_to_rtree(const Grid& grid) {
+    RTree<Point, double, DIMS> tree;
+    for (auto &el : grid) {
+      const double *pos = &el[0];
+      tree.Insert(pos, pos, el);
+    }
+    return tree;
+}
 
-// std::ostream &operator<<(std::ostream &os, const std::vector<int> &vec) {
-//   print_vec<int>(os, vec);
-//   return os;
-// }
-
-// std::ostream &operator<<(std::ostream &os, const std::vector<double> &vec) {
-//   print_vec<double>(os, vec);
-//   return os;
-// }
-
-// std::ostream &operator<<(std::ostream &os, const std::vector<std::vector<double>> &vec) {
-//   cerr << "{";
-//   for(const auto& x : vec) print_vec<double>(os, x);
-//   cerr << "}" << endl;
-//   return os;
-// }
-
-TEST_CASE("rtree test init")
+TEST_CASE("rtree basic ops")
 {
-
-#define DIMS 2
+  // this is needed as template param in RTree
+  // that's what I want to solve :)
+  #define dims 2
   int length = 4;
-  auto grid2 = make_grid(length, DIMS);
+  auto grid = make_grid(length, dims);
+  int size_init = pow(length, dims);
 
-  using Type = std::vector<double>;
-  RTree<Type, double, DIMS> tree;
+  RTree<Point, double, dims> tree = grid_to_rtree<dims>(grid);
 
-  for(auto& el : grid2) {
-    double* pos = &el[0];
-    cerr << "inserting " << pp(el) << endl;
-    tree.Insert(pos, pos, el);
-  }
-  REQUIRE(tree.Count() == pow(length, DIMS));
+  REQUIRE(tree.Count() == pow(length, dims));
 
-  std::vector<Type> found;
-  using Callback =  std::function<bool(Type, const double*,const double*)>;
-  Callback cb = [&found](Type node, const double* low, const double* high) {
-    // cerr << "callback! " << pp(node) << endl;
+  std::vector<Point> found;
+  using Callback =  std::function<bool(Point, const double*,const double*)>;
+  Callback cb = [&found](Point node, const double* low, const double* high) {
     found.push_back(node);
     return true;
   };
-
-  double low[DIMS] = {1,1};
-  double high[DIMS] = {1,3};
-  // we should get 1,1 1,2 1,3
+  
+  double low[dims] = {1,1};
+  double high[dims] = {1,3};
+  
   tree.Search(low, high, cb);
   REQUIRE(found.size() == 3);
-  cerr << "found " << pp(found) << endl;
-  std::sort(found.begin(), found.end(),
-            [](const Type &a, const Type &b) {
-              const auto size = std::min(a.size(), b.size());
-              for(auto i=0; i < size; i++) {
-                if ( a[i] < b[i]) {
-                  return true;
-                }
-              }
-              return false;
-            });
-  cerr << "sorted " << pp(found) << endl;
-  std::vector<Type> expected = {{1,1}, {1,2}, {1,3}};
-  REQUIRE(found == expected);
   
-  cerr << "done?\n";
+  std::vector<Point> expected = {{1,1}, {1,2}, {1,3}};
+  REQUIRE_THAT(found, Catch::Matchers::UnorderedEquals(expected));
+
+  int remove_cb_calls = 0;
+  Callback remove_cb = [&remove_cb_calls](Point node, const double *low, const double *high) {
+    remove_cb_calls++;
+    return true;
+  };
+  { 
+    double low[dims] = {1,1};
+    double high[dims] = {2,2};
+    int remove_count = tree.Remove(low, high, remove_cb);
+    REQUIRE(remove_count == 4);
+  }
+  REQUIRE(remove_cb_calls == 4);
+
+  found.clear();
+  tree.Search(low, high, cb);
+  REQUIRE(found.size() == 1);
+
+  expected = {{1,3}};
+  REQUIRE_THAT(found, Catch::Matchers::UnorderedEquals(expected));
+  REQUIRE(tree.Count() == size_init - 4);
+  
 }
